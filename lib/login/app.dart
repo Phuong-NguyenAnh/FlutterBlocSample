6aa7557b58ee519649775e7257c8ad20522ec868
Phuong Nguyen Anh <phuong.nguyenanh@gameloft.com>
2019-11-01 16:54:18 +0700

    import path

--- lib/login/app.dart
+++ lib/login/app.dart
@@ -2,11 +2,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:goplayers/infinite_list/home_page.dart';
-import 'package:goplayers/login/authentication/authentication.dart';
-import 'package:goplayers/login/loading_indicator.dart';
-import 'package:goplayers/login/login_page.dart';
-import 'package:goplayers/login/splash_page.dart';
+
+import 'home_page.dart';
+import 'authentication/authentication.dart';
+import 'loading_indicator.dart';
+import 'login_page.dart';
+import 'splash_page.dart';
 
 import 'bloc_delegate.dart';
 import 'user_repository.dart';
